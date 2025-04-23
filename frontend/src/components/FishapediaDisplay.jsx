375ccabcca8ceafbd37c93613f1ac50dead4b70a
Logan Lucas <logs10658@gmail.com>
2025-04-23 13:22:33 -0500

    modified fishapedia display to show rarities

--- frontend/src/components/FishapediaDisplay.jsx
+++ frontend/src/components/FishapediaDisplay.jsx
@@ -21,17 +21,35 @@ const FishapediaDisplay = ({ fishList }) => {
 
 // used to display a single fish inside of the fishapedia
 const Fish = ({ fishData }) => {
+    const rarityColor = {
+        Common: 'neutral-500',
+        Rare: 'emerald-500',
+        Epic: 'purple-500',
+        Legendary: 'amber-400',
+        Mythical: 'rose-400',
+    };
+    const selectedRarityColor = rarityColor[fishData.rarities];
+
+    const rarityBorderStyling = `border-${selectedRarityColor}`;
+
     return (
-        <>
-            <div className="flex bg-[var(--menu-light)] text-[var(--water-dark)] border-2 border-[var(--water-dark)] p-4 rounded-xl w-1/4 aspect-square text-center items-center justify-center shadow-lg">
-                <div className="flex flex-col">
-                    <span className="text-lg font-bold">
+        <div className="flex flex-col items-center">
+            <div
+                className={`flex bg-[var(--menu-light)] text-[var(--water-dark)] border-2 ${rarityBorderStyling} p-4 rounded-xl w-36 aspect-square shadow-lg`}
+            >
+                <div className="flex flex-col w-full items-center justify-center text-center gap-2">
+                    <span className="text-xl font-bold">
                         {fishData.fishname}
                     </span>
-                    <span># caught: {fishData.quantity}</span>
+                    <span className="text-sm">
+                        # caught: {fishData.quantity}
+                    </span>
                 </div>
             </div>
-        </>
+            <span className={`text-${selectedRarityColor} text-lg`}>
+                {fishData.rarities}
+            </span>
+        </div>
     );
 };
 
