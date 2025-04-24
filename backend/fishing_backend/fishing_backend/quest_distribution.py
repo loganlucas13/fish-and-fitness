190060f8f5e16c19d1303888ff88496f37eb90af
Logan Lucas <logs10658@gmail.com>
2025-04-24 01:10:01 -0500

    added quest distribution file

---
+++ backend/fishing_backend/fishing_backend/quest_distribution.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def get_random_quest():
+    return
+
+@csrf_exempt
+def request_quest(request):
+    return
